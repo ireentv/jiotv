f3f9eb3483367bd35a59cee07f927343a76543e4
Ireen TV <ireentvofficial@gmail.com>
2026-08-24 21:25:08 +0600

    Update jtvplus3.py
    4t444

--- jtvplus3.py
+++ jtvplus3.py
@@ -1,6 +1,6 @@
 import urllib.request
 import urllib.error
-url = 'https://jio-tv.sm-monirul-islam-rs.workers.dev/live'
+url = 'https://hotstarplugx.cstds.workers.dev/'
 # We MUST use a specific User-Agent like OTT Navigator, TiviMate, or okhttp to get the playlist
 headers = {
     'User-Agent': 'OTT Navigator'
